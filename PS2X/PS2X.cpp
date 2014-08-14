207a079901dcb33894e2cd327181720cafeb62e5
Konstantin <inferno2minus@gmail.com>
2014-08-14 21:45:51 +0600

    Первый коммит

---
+++ PS2X/PS2X.cpp
@@ -0,0 +1,127 @@
+/**
+ * Project PS2 Library
+ * Description: PS2 controller library for Arduino
+ * Version: v2.0
+ * Programmer: Bill Porter
+ *             Kompanets Konstantin (aka I2M)
+ *
+ * Hardware setup: Playstation 2 gamepad
+ */
+
+#include "PS2X.h"
+
+static uint8_t enter_config[] = { 0x01, 0x43, 0x00, 0x01, 0x00 };
+static uint8_t analog_mode[] = { 0x01, 0x44, 0x00, 0x01, 0x03, 0x00, 0x00, 0x00, 0x00 };
+static uint8_t exit_config[] = { 0x01, 0x43, 0x00, 0x00, 0x5A, 0x5A, 0x5A, 0x5A, 0x5A };
+
+uint16_t PS2X::Buttons() {
+  return ~_buttons;
+}
+
+uint8_t PS2X::NewButtonState() {
+  return ((_last_buttons ^ _buttons) > 0);
+}
+
+uint8_t PS2X::NewButtonState(uint16_t button) {
+  return (((_last_buttons ^ _buttons) & button) > 0);
+}
+
+uint8_t PS2X::ButtonPressed(uint16_t button) {
+  return(NewButtonState(button) & Button(button));
+}
+
+uint8_t PS2X::ButtonReleased(uint16_t button) {
+  return((NewButtonState(button)) & ((~_last_buttons & button) > 0));
+}
+
+uint8_t PS2X::Button(uint16_t button) {
+  return ((~_buttons & button) > 0);
+}
+
+uint8_t PS2X::Analog(uint8_t button) {
+  return _data[button];
+}
+
+uint8_t PS2X::read_gamepad() {
+  while (true) {
+    digitalWrite(_cmd, HIGH);
+    digitalWrite(_clk, HIGH);
+    digitalWrite(_att, LOW);
+
+    _data[0] = 0x01;
+    _data[1] = 0x42;
+
+    for (uint8_t i = 0; i < 21; i++) {
+     _data[i] = gamepad_shift(_data[i]);
+    }
+
+    digitalWrite(_att, HIGH);
+
+    if ((_data[1] & 0xf0) == 0x70) {
+      break;
+    }
+
+    reconfig_gamepad();
+  }
+
+   _last_buttons = _buttons;
+   _buttons = *(uint16_t*)(_data + 3);
+
+   return ((_data[1] & 0xf0) == 0x70);
+}
+
+void PS2X::config_gamepad(uint8_t clk, uint8_t cmd, uint8_t att, uint8_t dat) {
+  _clk = clk;
+  _cmd = cmd;
+  _att = att;
+  _dat = dat;
+
+  pinMode(_clk, OUTPUT);
+  pinMode(_att, OUTPUT);
+  pinMode(_cmd, OUTPUT);
+  pinMode(_dat, INPUT);
+
+  digitalWrite(_dat, HIGH);
+
+  read_gamepad();
+}
+
+uint8_t PS2X::gamepad_shift(uint8_t transmit_byte) {
+  uint8_t received_byte = 0;
+  for(uint8_t i = 0; i < 8; i++) {
+    if(transmit_byte & _BV(i)) {
+      digitalWrite(_cmd, HIGH);
+    }
+    else {
+      digitalWrite(_cmd, LOW);
+    }
+
+    digitalWrite(_clk, LOW);
+    delayMicroseconds(1);
+
+    if (digitalRead(_dat)) {
+      received_byte |= _BV(i);
+    }
+
+    digitalWrite(_clk, HIGH);
+  }
+
+  digitalWrite(_cmd, HIGH);
+  delayMicroseconds(10);
+
+  return received_byte;
+}
+
+void PS2X::send_command(uint8_t send_data[], uint8_t size) {
+  digitalWrite(_att, LOW);
+  for (uint8_t i = 0; i < size; i++) {
+    gamepad_shift(send_data[i]);
+  }
+  digitalWrite(_att, HIGH);
+}
+
+void PS2X::reconfig_gamepad() {
+  send_command(enter_config, sizeof(enter_config));
+  send_command(analog_mode, sizeof(analog_mode));
+  send_command(exit_config, sizeof(exit_config));
+}
---
+++ PS2X/PS2X.h
@@ -0,0 +1,86 @@
+/**
+ * Project PS2 Library
+ * Description: PS2 controller library for Arduino
+ * Version: v2.0
+ * Programmer: Bill Porter
+ *             Kompanets Konstantin (aka I2M)
+ *
+ * Hardware setup: Playstation 2 gamepad
+ */
+
+#ifndef PS2X_H
+#define PS2X_H
+
+#include "Arduino.h"
+
+//These are our button constants
+#define PSB_SELECT         0x0001
+#define PSB_L3             0x0002
+#define PSB_R3             0x0004
+#define PSB_START          0x0008
+#define PSB_PAD_UP         0x0010
+#define PSB_PAD_RIGHT      0x0020
+#define PSB_PAD_DOWN       0x0040
+#define PSB_PAD_LEFT       0x0080
+#define PSB_L2             0x0100
+#define PSB_R2             0x0200
+#define PSB_L1             0x0400
+#define PSB_R1             0x0800
+#define PSB_GREEN          0x1000
+#define PSB_RED            0x2000
+#define PSB_BLUE           0x4000
+#define PSB_PINK           0x8000
+#define PSB_TRIANGLE       0x1000
+#define PSB_CIRCLE         0x2000
+#define PSB_CROSS          0x4000
+#define PSB_SQUARE         0x8000
+
+//These are stick values
+#define PSS_RX             5
+#define PSS_RY             6
+#define PSS_LX             7
+#define PSS_LY             8
+
+//These are analog buttons
+#define PSAB_PAD_RIGHT     9
+#define PSAB_PAD_UP        11
+#define PSAB_PAD_DOWN      12
+#define PSAB_PAD_LEFT      10
+#define PSAB_L2            19
+#define PSAB_R2            20
+#define PSAB_L1            17
+#define PSAB_R1            18
+#define PSAB_GREEN         13
+#define PSAB_RED           14
+#define PSAB_BLUE          15
+#define PSAB_PINK          16
+#define PSAB_TRIANGLE      13
+#define PSAB_CIRCLE        14
+#define PSAB_CROSS         15
+#define PSAB_SQUARE        16
+
+class PS2X {
+  public:
+    uint16_t Buttons();
+    uint8_t NewButtonState();
+    uint8_t NewButtonState(uint16_t);
+    uint8_t ButtonPressed(uint16_t);
+    uint8_t ButtonReleased(uint16_t);
+    uint8_t Button(uint16_t);
+    uint8_t Analog(uint8_t);
+    uint8_t read_gamepad();
+    void config_gamepad(uint8_t, uint8_t, uint8_t, uint8_t);
+  private:
+    uint16_t _last_buttons;
+    uint16_t _buttons;
+    uint8_t _data[21];
+    uint8_t _clk;
+    uint8_t _cmd;
+    uint8_t _att;
+    uint8_t _dat;
+    uint8_t gamepad_shift(uint8_t);
+    void send_command(uint8_t*, uint8_t);
+    void reconfig_gamepad();
+};
+
+#endif
