bdd5dd3653dac244b97802e9815e3a18cf3bb1cc
Konstantin <inferno2minus@gmail.com>
2015-08-11 15:25:35 +0500

    Форматирование кода

--- PS2X.h
+++ PS2X.h
@@ -13,34 +13,34 @@
 
 #include "Arduino.h"
 
-#define BYTE_DELAY         16
-#define ATT_DELAY          16000
+#define BYTE_DELAY       16
+#define ATT_DELAY        16000
 
-#define PSB_SELECT         0x0001
-#define PSB_L3             0x0002
-#define PSB_R3             0x0004
-#define PSB_START          0x0008
-#define PSB_PAD_UP         0x0010
-#define PSB_PAD_RIGHT      0x0020
-#define PSB_PAD_DOWN       0x0040
-#define PSB_PAD_LEFT       0x0080
-#define PSB_L2             0x0100
-#define PSB_R2             0x0200
-#define PSB_L1             0x0400
-#define PSB_R1             0x0800
-#define PSB_GREEN          0x1000
-#define PSB_RED            0x2000
-#define PSB_BLUE           0x4000
-#define PSB_PINK           0x8000
-#define PSB_TRIANGLE       0x1000
-#define PSB_CIRCLE         0x2000
-#define PSB_CROSS          0x4000
-#define PSB_SQUARE         0x8000
+#define PSB_SELECT       0x0001
+#define PSB_L3           0x0002
+#define PSB_R3           0x0004
+#define PSB_START        0x0008
+#define PSB_PAD_UP       0x0010
+#define PSB_PAD_RIGHT    0x0020
+#define PSB_PAD_DOWN     0x0040
+#define PSB_PAD_LEFT     0x0080
+#define PSB_L2           0x0100
+#define PSB_R2           0x0200
+#define PSB_L1           0x0400
+#define PSB_R1           0x0800
+#define PSB_GREEN        0x1000
+#define PSB_RED          0x2000
+#define PSB_BLUE         0x4000
+#define PSB_PINK         0x8000
+#define PSB_TRIANGLE     0x1000
+#define PSB_CIRCLE       0x2000
+#define PSB_CROSS        0x4000
+#define PSB_SQUARE       0x8000
 
-#define PSS_RX             5
-#define PSS_RY             6
-#define PSS_LX             7
-#define PSS_LY             8
+#define PSS_RX           5
+#define PSS_RY           6
+#define PSS_LX           7
+#define PSS_LY           8
 
 class PS2X {
 public:
